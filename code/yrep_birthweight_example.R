a7b79f590661795935f5500e65e0277748503ddf
MJAlexander <monicaalexander@berkeley.edu>
2020-02-26 16:37:18 -0500

    added a plot of PPD and data

--- code/yrep_birthweight_example.R
+++ code/yrep_birthweight_example.R
@@ -17,6 +17,8 @@ N <- length(log_gest_c)
 # load in model output for Model 1
 
 load(here("output", "mod1.Rda"))
+
+# extract the samples
 fit <- extract(mod1)
 
 nsims <- dim(fit$beta)[1]
@@ -25,13 +27,16 @@ beta0_s <- fit$beta[,1]
 beta1_s <- fit$beta[,2]
 sigma_s <- fit$sigma
 
+# empty matrix to store samples
 log_weight_rep <- matrix(NA, nrow = N, ncol = nsims)
 
+# do the thing
 for(i in 1:N){
   log_weight_rep[i,] <- rnorm(nsims, mean = beta0_s+beta1_s*log_gest_c[i], sd = sigma_s)
 }
 
 
+# data easier to plot
 colnames(log_weight_rep) <- 1:nsims
 
 dr <- as_tibble(log_weight_rep)
@@ -39,4 +44,20 @@ dr <- dr %>% bind_cols(i = 1:N, log_weight_obs = log(ds$birthweight))
 
 dr <- dr %>% 
   pivot_longer(`1`:`1000`, names_to = "sim", values_to = "log_weight_rep")
+
+# plot densities for 100 samples
+
+set.seed(176)
+sims_to_sample <- sample(1:nsims, 100)
+
+dr %>% 
+  filter(sim %in% sims_to_sample) %>% 
+  ggplot(aes(log_weight_rep, group = sim)) + 
+  geom_density(alpha = 0.2, aes(color = "y_rep")) + 
+  geom_density(data = ds %>% mutate(sim = 1), 
+               aes(x = log(birthweight), col = "y")) + 
+  scale_color_manual(name = "", 
+                     values = c("y" = "darkblue", "y_rep" = "lightblue"))
+
+
 
